d502ac5c2ee538928f59fe4a02642a06b6b72290
Hakimix <mustafa.hakimi@outlook.dk>
2022-08-14 15:22:19 +0200

    add stack and update readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # lambda-s3-trigger
+
+Send notification to Lambda function whenever new objects are created in S3 bucket
+
+![](resources/lambda-s3-trigger-overview.png)
--- lambda_s3_trigger/lambda_s3_trigger_stack.py
+++ lambda_s3_trigger/lambda_s3_trigger_stack.py
@@ -1,7 +1,8 @@
 from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
+    aws_lambda as _lambda,
+    aws_s3 as _s3,
+    aws_s3_notifications as s3_notification,
+    Stack
 )
 from constructs import Construct
 
@@ -9,11 +10,20 @@ class LambdaS3TriggerStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+        
+        # Lambda function
+        lambda_function = _lambda.Function(
+            self, 'lambda_function',
+            runtime=_lambda.Runtime.PYTHON_3_8,
+            handler="lambda.handler",
+            code=_lambda.Code.from_asset('./lambda')
+        )
 
-        # The code that defines your stack goes here
+        # S3 bucket
+        s3_bucket = _s3.Bucket(self, 's3_bucket')
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "LambdaS3TriggerQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # S3 notification for lambda function
+        notification = s3_notification.LambdaDestination(lambda_function)
+
+        # assign notification for the s3 event type (ex: OBJECT_CREATED)
+        s3_bucket.add_event_notification(_s3.EventType.OBJECT_CREATED, notification)
